d5816317845214f885d4389721722b05aa50fcf2
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-06-26 19:47:06 -0300

    thread pool

--- demo/src-webview/src/portal/mod.rs
+++ demo/src-webview/src/portal/mod.rs
@@ -17,10 +17,10 @@ pub fn run_async<T: 'static, F: FnOnce() -> Result<String, String> + Send + 'sta
     let handle = webview.handle();
      POOL.with(|thread| {
         thread.execute(move || {
-        let callback_string = rpc::format_callback_result(what(), callback, error);
-        handle.dispatch(move |_webview| {
-            _webview.eval(callback_string.as_str())
-        }).unwrap()
+            let callback_string = rpc::format_callback_result(what(), callback, error);
+            handle.dispatch(move |_webview| {
+                _webview.eval(callback_string.as_str())
+            }).unwrap()
     });
     });
 }
--- src/rust/base/src-webview/src/portal/mod.rs
+++ src/rust/base/src-webview/src/portal/mod.rs
@@ -1,3 +1,5 @@
+extern crate threadpool;
+
 pub mod command;
 pub mod file_system;
 pub mod dir;
@@ -7,14 +9,18 @@ pub mod rpc;
 extern crate web_view;
 use web_view::WebView;
 
-use std::thread;
+use threadpool::ThreadPool;
+
+thread_local!(static POOL: ThreadPool = ThreadPool::new(4));
 
 pub fn run_async<T: 'static, F: FnOnce() -> Result<String, String> + Send + 'static>(webview: &mut WebView<T>, what: F, callback: String, error: String) {
     let handle = webview.handle();
-    thread::spawn(move || {
-        let callback_string = rpc::format_callback_result(what(), callback, error);
-        handle.dispatch(move |_webview| {
-            _webview.eval(callback_string.as_str())
-        }).unwrap()
+     POOL.with(|thread| {
+        thread.execute(move || {
+            let callback_string = rpc::format_callback_result(what(), callback, error);
+            handle.dispatch(move |_webview| {
+                _webview.eval(callback_string.as_str())
+            }).unwrap()
+    });
     });
 }
